ad8741b7beb8cb02cee92660086c10cd859e87a2
Ryan Frazier <frazierr2@gmail.com>
2016-10-12 15:08:09 -0400

    Created failed tests function

--- tests/tests.js
+++ tests/tests.js
@@ -8,15 +8,15 @@ var Post = require('../app/scripts/models').Post;
 // ################################################
 // Model Tests
 // ################################################
-describe('Post', function(){
-  describe('fetch', function(){
-    it('should return a promise', function(){
+describe('Post', function() {
+  describe('fetch', function() {
+    it('should return a promise', function() {
       var promise = Post.fetch();
       expect(promise).to.respondTo('then');
     });
 
-    it('should resolve with an array of posts', function(done){
-      Post.fetch().then(function(posts){
+    it('should resolve with an array of posts', function(done) {
+      Post.fetch().then(function(posts) {
         var firstPost = posts[0];
         expect(firstPost).to.have.property('title');
         expect(firstPost).to.have.property('body');
@@ -26,9 +26,9 @@ describe('Post', function(){
       });
     });
 
-    it("should trigger a posts:fetched event", function(done){
+    it("should trigger a posts:fetched event", function(done) {
 
-      $(document).on('posts:fetched', function(event, posts){
+      $(document).on('posts:fetched', function(event, posts) {
         expect(posts).to.be.an.instanceof(Array);
         done();
       });
@@ -41,20 +41,37 @@ describe('Post', function(){
 // ##############################
 // View Tests
 // ##############################
-describe("PostView", function(){
+describe("PostView", function() {
   var view, posts;
 
-  beforeEach(function(){
-    posts = [{title: "Title", body: "Body"}];
+  beforeEach(function() {
+    posts = [{
+      title: "Title",
+      body: "Body"
+    }];
     view = new PostView();
   });
 
-  describe("showPosts", function(){
-    it("should take a post array and list them", function(){
+  describe("showPosts", function() {
+    it("should take a post array and list them", function() {
       view.showPosts(posts);
       expect($('.posts li').length).to.equal(1);
       expect($('.posts li h1').text()).to.equal("Title");
       expect($('.posts li p').text()).to.equal("Body");
     });
+  });
+});
+
+describe("createPostForm", function() {
+  it('should trigger a create:post event on the document with the title and body', function(done) {
+    $(document).on('create:post', function(event, post) {
+      expect(post).to.have.property('title');
+      expect(post).to.have.property('body');
+      done();
+    });
+    $('.input-class-name').val("title");
+    $('.input-class-name').val("body");
+    $("form-button").click();
+
   });
 });
